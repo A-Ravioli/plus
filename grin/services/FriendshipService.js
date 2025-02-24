ea1aac0cd8ac5308c1cdb5de7b676b027025ef55
Arav Kumar <68564296+A-Ravioli@users.noreply.github.com>
2025-02-23 21:19:36 -0500

    restarting

--- grin/services/FriendshipService.js
+++
@@ -1,8 +0,0 @@
-const FriendshipService = {
-  getMutualFriendsPosts: async (allPosts, currentUsername) => {
-    // Dummy implementation: return all posts, in a real scenario, filter based on mutual friendship
-    return allPosts;
-  }
-};
-
-export default FriendshipService; 
